4c5a0414fb04cda37a5475ebed77a98543e03c93
coffmanjrp <coffmanjrp@gmail.com>
2021-03-17 16:02:18 -0700

    Fetch posts titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,42 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
 
-export default function Home() {
+const BLOG_URL = 'https://next-ghost-blog-backend.herokuapp.com';
+const CONTENT_API_KEY = 'bbe57347cf4334b9455cae967f';
+
+type Post = {};
+
+async function getPost() {
+  const res = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}`
+  ).then((res) => res.json());
+
+  const titles = res.posts.map((post) => post.title);
+
+  console.log(titles);
+
+  return titles;
+}
+
+export const getStaticProps = async ({ params }) => {
+  const posts = await getPost();
+
+  return {
+    props: { posts },
+  };
+};
+
+const Home: React.FC<{ posts: string[] }> = ({ posts }) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>Hello to my blog</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default Home;
